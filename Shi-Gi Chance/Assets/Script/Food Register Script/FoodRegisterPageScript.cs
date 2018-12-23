91e48ec75851fd550111ace6ba29dc2e624beeb3
LusterFlame <kurasiki.homura@gmail.com>
2018-12-23 15:42:46 +0900

    minor changes

---
+++ Shi-Gi Chance/Assets/Script/Food Register Script/FoodRegisterPageScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodRegisterPageScript : MonoBehaviour {
+
+	//////////////////
+	public GameObject MainUI;
+	//////////////////
+
+	// Use this for initialization before awake
+	void Awake()
+	{
+		transform.SetParent(MainUI.transform);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		ObjectResize();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		ObjectResize();
+	}
+
+	//////////////////
+	
+	void ObjectResize()
+	{
+		GetComponent<RectTransform>().sizeDelta = GetComponentInParent<RectTransform>().sizeDelta;
+	}
+}
