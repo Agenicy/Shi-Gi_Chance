ea60db7b24df60164141fc1783f16f64f5991644
Agenicy <45389507+Agenicy@users.noreply.github.com>
2018-12-24 20:05:58 +0800

    fix pict  order in layer

--- Shi-Gi Chance/Assets/Script/Building/Chess/CreateBuilding.cs
+++ Shi-Gi Chance/Assets/Script/Building/Chess/CreateBuilding.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Building
 {
@@ -29,24 +30,24 @@ public class CreateBuilding : MonoBehaviour {
 	
 	void OnTriggerEnter2D(Collider2D B)
 	{
-		this.gameObject.transform.GetComponent<SpriteRenderer>().color = new Color(this.gameObject.transform.GetComponent<SpriteRenderer>().color.r, this.gameObject.transform.GetComponent<SpriteRenderer>().color.g, this.gameObject.transform.GetComponent<SpriteRenderer>().color.b, 1f);
+		SparkleOff();
+		InvokeRepeating("Sparkle", 0f, 0.6f);
 	}
 
 	void OnTriggerStay2D(Collider2D B)
 	{
-		this.gameObject.transform.GetComponent<SpriteRenderer>().color = new Color(this.gameObject.transform.GetComponent<SpriteRenderer>().color.r, this.gameObject.transform.GetComponent<SpriteRenderer>().color.g, this.gameObject.transform.GetComponent<SpriteRenderer>().color.b, 0.5f);
-
 		if (B.gameObject.transform.GetComponent<DragChess>().isDrag == false)
 		{
 			BuildHouse(B);
 			GameObject.Destroy(B.gameObject);
-			this.gameObject.transform.GetComponent<SpriteRenderer>().color = new Color(this.gameObject.transform.GetComponent<SpriteRenderer>().color.r, this.gameObject.transform.GetComponent<SpriteRenderer>().color.g, this.gameObject.transform.GetComponent<SpriteRenderer>().color.b, 1f);
+			SparkleOff();
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D B)
 	{
-		this.gameObject.transform.GetComponent<SpriteRenderer>().color = new Color(this.gameObject.transform.GetComponent<SpriteRenderer>().color.r, this.gameObject.transform.GetComponent<SpriteRenderer>().color.g, this.gameObject.transform.GetComponent<SpriteRenderer>().color.b, 1f);
+		CancelInvoke("Sparkle");
+		SparkleOff();
 	}
 
 	//////////////////////
@@ -54,6 +55,7 @@ public class CreateBuilding : MonoBehaviour {
 	private void BuildHouse(Collider2D B)
 	{
 		//將透明度改回去
+		SparkleOff();
 
 		//製造建築模板
 		GameObject building = GameObject.Instantiate(BuildingTemplate, transform, false);
@@ -61,5 +63,17 @@ public class CreateBuilding : MonoBehaviour {
 		//從棋子ID取得所有建築物資訊
 		/***/
 
+	}
+
+	private void Sparkle()
+	{
+		this.gameObject.transform.GetComponent<SpriteRenderer>().color = new Color(this.gameObject.transform.GetComponent<SpriteRenderer>().color.r, this.gameObject.transform.GetComponent<SpriteRenderer>().color.g, this.gameObject.transform.GetComponent<SpriteRenderer>().color.b, 0.5f);
+
+		Invoke("SparkleOff", 0.3f);
+	}
+
+	private void SparkleOff()
+	{
+		this.gameObject.transform.GetComponent<SpriteRenderer>().color = new Color(this.gameObject.transform.GetComponent<SpriteRenderer>().color.r, this.gameObject.transform.GetComponent<SpriteRenderer>().color.g, this.gameObject.transform.GetComponent<SpriteRenderer>().color.b, 1f);
 	}
 }
