398b59fcc3aec7af8dfe385c35130fc1c911ea0b
LusterFlame <kurasiki.homura@gmail.com>
2018-12-24 22:55:10 +0900

    FR Button Adjusted

--- Shi-Gi Chance/Assets/Script/Main Screen Scripts/FoodRegisterButtonScript.cs
+++ Shi-Gi Chance/Assets/Script/Main Screen Scripts/FoodRegisterButtonScript.cs
@@ -12,13 +12,13 @@ public class FoodRegisterButtonScript : MonoBehaviour {
 	void Awake()
 	{
 		transform.SetParent(MainScreenPanel.transform);
+		ObjectResize();
 	}
 
 	// Use this for initialization
 	void Start ()
 	{
 		ObjectResize();
-		transform.SetAsLastSibling();
 	}
 	
 	// Update is called once per frame
@@ -32,5 +32,6 @@ public class FoodRegisterButtonScript : MonoBehaviour {
 	{
 		GetComponent<RectTransform>().sizeDelta = new Vector2((Screen.width - 100) / 4, (Screen.width - 100) / 4);
 		transform.localPosition = new Vector3(40 + (Screen.width - 100) / 4 - (Screen.width / 2) + (Screen.width - 100) / 8, 20 + (Screen.width - 100) / 4 - (Screen.height / 2) - (Screen.width - 100) / 8, 5);
+		transform.SetAsLastSibling();
 	}
 }
