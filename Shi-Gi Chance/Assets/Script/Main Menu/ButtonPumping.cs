d37d8a5bc2b7a2cafa72e33eeab5e3966e1f7b48
Agenicy <45389507+Agenicy@users.noreply.github.com>
2019-01-04 15:26:22 +0800

    Main Menu

---
+++ Shi-Gi Chance/Assets/Script/Main Menu/ButtonPumping.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPumping : MonoBehaviour {
+
+	[SerializeField] AudioClip Hover;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PumpIn()
+	{
+		transform.gameObject.GetComponent<RectTransform>().localScale = new Vector3(1.05f, 1.05f, 1);
+
+		GetComponent<AudioSource>().clip = Hover;
+		GetComponent<AudioSource>().Play();
+	}
+
+	public void PumpOut()
+	{
+		transform.gameObject.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1);
+	}
+}
---
+++ Shi-Gi Chance/Assets/Script/Main Menu/PumpingLogo.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpingLogo : MonoBehaviour
+{
+	[SerializeField] float BPM;
+
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("Bumping",0, 2*(float)60 / BPM);
+		GetComponent<AudioSource>().Play();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void Bumping()
+	{
+		transform.GetChild(0).gameObject.GetComponent<RectTransform>().localScale = new Vector3(1.05f, 1.05f, 1);
+		Invoke("BumpingOver", (float)60 / BPM);
+	}
+
+	private void BumpingOver()
+	{
+		transform.GetChild(0).gameObject.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+	}
+}
